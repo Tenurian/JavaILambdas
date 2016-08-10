e0374f2b8178d4a8c14867106e533bdd3e2a4269
Thomas Feuerborn <tfeuerbo@gmail.com>
2016-08-10 14:50:19 -0600

    Signed-off-by: Thomas Feuerborn <tfeuerbo@gmail.com>

--- src/stuff/MovieMain.java
+++ src/stuff/MovieMain.java
@@ -3,7 +3,9 @@ package stuff;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class MovieMain {
 
@@ -51,13 +53,51 @@ public class MovieMain {
 	}
 
 	public static void main(String[] args) {
-		MovieList allMovies = new MovieList(Arrays.asList(movies));
-		MovieList moviesBefore = new MovieList(getMoviesByLambda((m) -> (m.getYear() < 1991), allMovies));
-		MovieList moviesBeforeAndAfter = new MovieList(getMoviesByLambda((m) -> (m.getYear() > 1983), moviesBefore));
+		/*stream | intermediate | terminal*/
+//		List<Movie> moviesList = Arrays.asList(movies);
+//		moviesList.stream().forEach(  (m) -> System.out.println(m.getTitle())  );
+//		Arrays.stream(movies).forEach(  (m) -> System.out.println(m.getTitle())  );
+		/**===Example of a Pipeline====**/
+//		long count = 
+		/* C# equivalent of Optional<T> is "T? varName" */
+//		String titles = 
+		Stream.of(
+				new Movie("Raiders of the Lost Ark", 1981, 4.5f, "Adventure"),
+				new Movie("The Shining", 1984, 4f, "Horror"),
+				new Movie("Lion King", 1996, 4f, "Family"),
+				new Movie("Batman and Robin", 1998, 1f, "Action")
+				)											//stream
+//		.filter( (m) -> m.getYear() > 1983 )				//1st filter
+//		.filter( (m) -> m.getYear() < 1998 )				//2nd filter
+		.sorted( (m1, m2) -> m2.getYear()-m1.getYear() )	//sorting filter
+//		.limit(1)											//limiting filter
+//		.skip(1)											//skip n values and return results
+		/*  movie ->  String */
 		
-		for(Movie m : moviesBeforeAndAfter){
-			System.out.println(m.getTitle() + " ("+m.getYear()+")");
-		}
+		.map( (m) -> m.getStars() )							//convert the type from T -> Q
+		.distinct()											//get unique objects after filtering and sorting
+		.forEach( (t) -> System.out.println(t) );			//terminal that does expression
+//		.count();											//terminal that returns count of items
+//		.findFirst();										//terminal that gets the first item in the stream,
+															//usually follows a sorted filter
+//		.findAny();
+//		.reduce( "", (s1, s2) -> s1 + " | " + s2 ); //s1 = s1 + " | " + s2, repeat until empty
+//		System.out.println(titles);
+//		System.out.println(title.isPresent()?title.get():"");
+		
+		
+		
+		
+		
+//		MovieList allMovies = new MovieList(Arrays.asList(movies));
+//		MovieList moviesBefore = new MovieList(getMoviesByLambda((m) -> (m.getYear() < 1991), allMovies));
+//		MovieList moviesBeforeAndAfter = new MovieList(getMoviesByLambda((m) -> (m.getYear() > 1983), moviesBefore));
+//		
+//		for(Movie m : moviesBeforeAndAfter){
+//			System.out.println(m.getTitle() + " ("+m.getYear()+")");
+//		}
+		
+		
 		
 		
 		
